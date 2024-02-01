bc3946eb8a1ebe14af7b35addff0e2a9222aa6d3
codingWiz-rick <chakrabortyshubham66@gmail.com>
2024-02-01 15:57:13 +0530

    Initial commit

--- app/src/main/java/com/codewiz/sharedpreference/MainActivity.java
+++ app/src/main/java/com/codewiz/sharedpreference/MainActivity.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.codewiz.sharedpreference.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding inflate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        inflate = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(inflate.getRoot());
         SharedPreference sharedPreference = new SharedPreference(this);
     }
 }
